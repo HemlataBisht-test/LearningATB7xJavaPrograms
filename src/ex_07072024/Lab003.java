a1ed10f32cc106dea70e690bd1476ebde181fdea
BaneleSTS <banelen@stsafrica.com>
2024-07-09 09:21:58 +0530

    Adding a new file Lab004

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Testing Academy Automation Testing course !");
+    }
+}
---
+++ src/ex_07072024/Lab004.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Aurthmatical operation");
+        System.out.println("5+5");
+        System.out.println("5-5");
+        System.out.println("5*5");
+        System.out.println("5/5");
+
+
+    }
+}
