16eef3b5c422967620899fadab14235c2a312bf8
BaneleSTS <banelen@stsafrica.com>
2024-07-13 17:35:52 +0530

    Adding a new file Lab004

---
+++ src/ex_13072024/Lab005.java
@@ -0,0 +1,18 @@
+package ex_13072024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        // calculator program
+        double a=15;
+         double b= 25;
+         double sum = a+b;
+         double sub = a-b;
+         double multiply = a*b;
+         double div = a/b;
+        System.out.printf("Sum is %f\n",sum);
+        System.out.printf("Sub is %f\n",sub);
+        System.out.printf("Multiply is %f\n",multiply);
+        System.out.printf("Devison is %f\n",div);
+
+    }
+}
---
+++ src/ex_13072024/Lab006.java
@@ -0,0 +1,14 @@
+package ex_13072024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        int a= 7;
+        int b= 9;
+
+      String  name= "Aarya";
+        System.out.println(a+b);
+        System.out.println(a+b+ name);
+        System.out.println(a+name+b);
+        System.out.println(name+a+b);
+    }
+}
