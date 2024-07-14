50efe31df3569ba59d2c2e8a78d30d4fdb0a1024
BaneleSTS <banelen@stsafrica.com>
2024-07-14 15:27:19 +0530

    Adding a new file Lab009

---
+++ src/ex_14072024/Lab010.java
@@ -0,0 +1,10 @@
+package ex_14072024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        byte a= 10;
+        int b= 30;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
