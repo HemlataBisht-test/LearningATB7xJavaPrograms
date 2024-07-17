8d9cd7f95800a3d23f88726ff05596a8e3be7822
BaneleSTS <banelen@stsafrica.com>
2024-07-17 20:58:37 +0530

    Adding a new file Lab021-23

---
+++ src/ex_14072024/Lab021.java
@@ -0,0 +1,17 @@
+package ex_14072024;
+
+public class Lab021 {
+    public static void main(String[] args) {
+        String password ="Good@123";
+        String password_u =password.toLowerCase();
+        System.out.println(password_u);
+        // password == password_u   ans is no
+        System.out.println(password == password_u );   // false
+        System.out.println(password.equals(password_u) );  //false
+        System.out.println(password.equalsIgnoreCase(password_u) );  // true
+        System.out.println(password.length());
+        System.out.println(password.substring(0,4));
+        System.out.println(password.indexOf('d'));
+
+    }
+}
---
+++ src/ex_14072024/Lab023.java
@@ -0,0 +1,10 @@
+package ex_14072024;
+import java.lang.Math;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        double sq=Math.sqrt(2);
+        System.out.println(sq);
+
+    }
+}
