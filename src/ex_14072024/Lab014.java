7ff7911b0ae78f72af1cd00beb8b7b56f3d60323
BaneleSTS <banelen@stsafrica.com>
2024-07-17 19:36:01 +0530

    Adding a new file Lab011-16

---
+++ src/ex_14072024/Lab014.java
@@ -0,0 +1,18 @@
+package ex_14072024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        byte a= 10;
+        byte b = 23;
+        int c=a+b;    // byte + byte = int
+        System.out.println(c);
+
+
+        char d='A';
+        char e= 'B';
+        int f= d+e;     // char + char = int    Char converted in int due to ASCII code
+        System.out.println(f);  // Ans will be (65 + 66= 131) in ASCII code A= 65 and B =66
+        System.out.println(d==65);
+        System.out.println(e ==66);
+    }
+}
---
+++ src/ex_14072024/Lab016.java
@@ -0,0 +1,18 @@
+package ex_14072024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        long phoneNumber =7372626382L;
+        short a = (short)phoneNumber;
+        System.out.println(a);  // Explicit type casting
+
+        float b = 8747277.98776f;
+        long c= (long)b;
+        System.out.println(c);
+
+        float d=938283.998667892f;
+        double e= (double)d;
+        System.out.println(e);
+
+    }
+}
