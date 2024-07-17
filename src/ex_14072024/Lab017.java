b8b47a440004e9fbcb70cf39f4a83985dd4fcb6a
BaneleSTS <banelen@stsafrica.com>
2024-07-17 20:17:58 +0530

    Adding a new file Lab017-20

--- src/ex_14072024/Lab017.java
+++ src/ex_14072024/Lab017.java
@@ -1,4 +1,19 @@
 package ex_14072024;
 
 public class Lab017 {
+    public static void main(String[] args) {
+        // Increment (++) / Decrement (--) Operators
+        // (++a)Pre and (a++)Post
+        // pre - increment ++operand
+        // value is incremented first and then stored in the result.
+
+        int a=10;
+
+        // POST
+        //  value is stored in the result and incremented later
+
+       // System.out.println(++a + a++ + a++); // 11+11+12 = 34
+
+          System.out.println(a++ + ++a + ++a);    //10+12+13 =35
+    }
 }
