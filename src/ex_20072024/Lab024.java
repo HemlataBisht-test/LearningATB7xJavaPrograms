ca04feacf594c96fb90d1d146fdba3d9b8b05149
BaneleSTS <banelen@stsafrica.com>
2024-07-20 11:25:50 +0530

    Adding a new file Lab024

---
+++ src/ex_20072024/Lab024.java
@@ -0,0 +1,26 @@
+package ex_20072024;
+
+public class Lab024 {
+    public static void main(String[] args) {
+        // Condition and Loops
+        //
+        // Condition ?
+        // age  > 18 -> you allowed to / Vote
+        //
+        //  if age > 18 -> vote, else -> not allowed.
+
+        // Condition - Java -> switch , If
+        // syntax
+//        if (condition ==> true or false){
+//
+//        }else{
+//
+//        }
+        int age = 15;
+        if (age>=18){
+            System.out.println("You are eligible for voting ");
+
+        } else
+            System.out.println("You are not eligible for voting");
+    }
+}
