d774616d775b1bf530a978168e07c081ea886809
BaneleSTS <banelen@stsafrica.com>
2024-07-20 19:11:47 +0530

    Adding a new file Lab041-46

---
+++ src/ex_20072024/Lab042.java
@@ -0,0 +1,17 @@
+package ex_20072024;
+
+public class Lab042 {
+    public static void main(String[] args) {
+        // JDK > 13
+        char code = 'B';
+      /*  int val = switch (code){
+            case 'A':
+                yield 65;
+            case 'B':
+                yield 66;//// return
+            default:
+                System.out.println("Unexpected value: " + code);
+        } */
+      //  System.out.println(val);
+    }
+}
