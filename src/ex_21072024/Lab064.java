5ea4b2b466137f3105b584078adf2b61f0e268a0
BaneleSTS <banelen@stsafrica.com>
2024-07-24 21:24:08 +0530

    Adding a new file Lab059-66

---
+++ src/ex_21072024/Lab064.java
@@ -0,0 +1,9 @@
+package ex_21072024;
+
+public class Lab064 {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("inside the while loop");  // infinite because condition is always true
+        }
+    }
+}
