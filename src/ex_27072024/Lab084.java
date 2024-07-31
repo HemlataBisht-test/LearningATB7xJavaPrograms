ebf750f08437ce43b3b10f0407819c568cbb5c43
BaneleSTS <banelen@stsafrica.com>
2024-07-31 21:08:44 +0530

    Adding a new file Lab084 homework 2nd max salary

---
+++ src/ex_27072024/Lab084.java
@@ -0,0 +1,10 @@
+package ex_27072024;
+
+public class Lab084 {
+    public static void main(String[] args) {
+        test1();
+    }
+    static void test1(){
+        System.out.println("I am inside the function :");
+    }
+}
