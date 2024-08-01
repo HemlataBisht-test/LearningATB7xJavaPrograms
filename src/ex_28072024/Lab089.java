0f253de360bcbec7d6ac62b9ce0180e3f5760e09
BaneleSTS <banelen@stsafrica.com>
2024-08-01 16:52:47 +0530

    Adding a new file Lab088-90 OOPs concept

---
+++ src/ex_28072024/Lab089.java
@@ -0,0 +1,33 @@
+package ex_28072024;
+
+public class Lab089 {
+    public static void main(String[] args) {
+        //  Class - Attribute / properties - data Types - primitive or non primitive.
+        // Class - Behaviour /Methods - functions -
+        // object - real entity - class -
+        // ClassName classRef = new ClassName();  //creating an object
+        // new ClassName(); - Object
+        // classRef - Class Ref
+        // ClassName = Class Name which blueprint you are using
+     BuildingBP gaur = new BuildingBP();
+     gaur.name = "Gaur city";
+     gaur.colour ="Pink";
+     gaur.lifts=2;
+     gaur.address="Noida";
+     gaur.noOfRooms=15;
+
+     gaur.useLift();
+     gaur.activities();
+
+        BuildingBP ajnara = new BuildingBP();
+        ajnara.name = "Ajnara Homes";
+        ajnara.colour ="Pink";
+        ajnara.lifts=2;
+        ajnara.address="Noida";
+        ajnara.noOfRooms=15;
+
+        ajnara.activities();
+        System.out.println(ajnara.name);
+        System.out.println(gaur.name);
+    }
+}
---
+++ src/ex_28072024/Person.java
@@ -0,0 +1,25 @@
+package ex_28072024;
+
+public class Person {
+    String Name;
+    int age;
+    String gender;
+    int rollno;
+    String colour;
+
+    static  void walk(){
+        System.out.println("walking ");
+    }
+    static  void eat(){
+        System.out.println("eating ");
+    }
+    static  void talk(){
+        System.out.println("talking ");
+    }
+    static  void sing(){
+        System.out.println("singing ");
+    }
+    static  void sleep(){
+        System.out.println("sleeping ");
+    }
+}
